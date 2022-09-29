1b9ebdf1fc001fe4f1eef7703aca83a9c113e0a4
TriptoAfsin <AfsinTripto@gmail.com>
2022-09-29 23:11:28 +0600

    added cors config

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
 //importing web routes
-const initWebRoutes = require('./routes/web');
+const initWebRoutes = require("./routes/web");
 //importing api controller
-const apiController = require('./controllers/apiController');
+const apiController = require("./controllers/apiController");
 
 //port
-const port = process.env.PORT
-
+const port = process.env.PORT;
 
 //init web routes
 initWebRoutes(app);
 
 //cors middleware
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.listen(port, () => {
-    console.log(`🔵 Listening on Port: ${port}`)
-})
+  console.log(`🔵 Listening on Port: ${port}`);
+});
