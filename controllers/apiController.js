c0663ca257f1f496b0258018816c44797f1f2d46
Afshin Nahian Tripto <afsintripto@gmail.com>
2023-07-08 16:13:52 +0600

    fix: prod update fix

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -116,14 +116,14 @@ let showAllFromTable = tableName => {
 
 let updateProductById = (id, data) => {
   return `UPDATE products_thesis SET ${
-    data?.style ? `style = '${data?.style}'` : ""
-  },${data?.total_qty ? `total_qty = '${data?.total_qty}'` : ""},${
-    data?.type ? `type = '${data?.type}'` : ""
-  },${data?.color ? `color = '${data?.color}'` : ""},${
-    data?.name ? `name = '${data?.name}'` : ""
-  },${data?.po ? `po = '${data?.po}'` : ""},${
-    data?.other_info ? `other_info = '${data?.other_info}'` : ""
-  }, WHERE products_thesis.product_id = ${id}`;
+    data?.style ? `style = '${data?.style}',` : ""
+  }${data?.total_qty ? `total_qty = '${data?.total_qty}'` : ""},${
+    data?.type ? `type = '${data?.type}',` : ""
+  }${data?.color ? `color = '${data?.color}',` : ""},${
+    data?.name ? `name = '${data?.name}',` : ""
+  }${data?.po ? `po = '${data?.po}',` : ""}${
+    data?.other_info ? `other_info = '${data?.other_info}',` : ""
+  } WHERE products_thesis.product_id = ${id}`;
 };
 
 let updateTransactionById = (id, data) => {
